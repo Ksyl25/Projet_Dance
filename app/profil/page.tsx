cb2293cb8c8cd60318d5310f7fd82ebc30fb3dbd
MatthieuStephan <150937528+MatthieuStephan@users.noreply.github.com>
2024-06-05 17:39:02 +0100

    Répertoir profil et navbar
    Répertoir profil et navbar

---
+++ app/profil/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+
+
+
+/*const profil = () => {
+  return (
+    <div>profil</div>
+  )
+}
+
+export default profil*/
+
+import Navbar from "../../Components/NavBar";
+function App() {
+	return (
+		<React.Fragment>
+			<Navbar/>
+		</React.Fragment>
+	);
+}
+
+export default App;
